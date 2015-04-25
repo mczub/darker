1d9dce2a025b6c60db099ff56b71f32cd4c35190
mczub <mczub@outlook.com>
2015-04-25 00:00:12 -0700

    added click to cycle

--- script/background.js
+++ script/background.js
@@ -1,7 +1,7 @@
-var WIDTH = 1200;
-var HEIGHT = 1200;
-var NUM_X = 6;
-var NUM_Y = 6;
+var WIDTH = 1600;
+var HEIGHT = 1600;
+var NUM_X = 8;
+var NUM_Y = 8;
 var canvas = document.createElement('canvas');
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
@@ -15,15 +15,19 @@ var textures = new Array(
 );
 
 
-var styleArray = new Array([HEIGHT]);
-for (var i = 0; i < styleArray.length; i++)
+var styleArray = new Array();
+for (var i = 0; i < NUM_X; i++)
 {
-	styleArray[i] = new Array([WIDTH]);
+	styleArray[i] = new Array();
+	for (var j = 0; j < NUM_Y; j++)
+	{
+		styleArray[i][j] = Math.floor((Math.random() * textures.length));
+	}
 }
 function checkAllTexturesLoaded()
 {
 	texturesReady++;
-	if (texturesReady >= 4)
+	if (texturesReady >= textures.length)
 	{
 		drawGlyphMask();
 		drawImages();
@@ -38,10 +42,23 @@ for(var i = 0; i < textures.length; i++)
 		checkAllTexturesLoaded();
 	}
 }
-textures[0].src = "/white.jpg";
-textures[1].src = "/darkgray.jpg";
-textures[2].src = "/brick.jpg";
-textures[3].src = "/grass.jpg";
+textures[0].src = "./white.jpg";
+textures[1].src = "./darkgray.jpg";
+textures[2].src = "./brick.jpg";
+textures[3].src = "./grass.jpg";
+
+var elem = document.getElementsByClassName("intro")[0];
+elem.addEventListener('click', function(event)
+{
+	//console.log(event.pageX + "," + event.pageY);
+	//console.log(elem.offsetWidth + "," + elem.offsetHeight);
+	var canv_x_pos = (WIDTH - elem.offsetWidth) / 2 + event.pageX;
+	var canv_y_pos = (HEIGHT - elem.offsetHeight) / 2 + event.pageY;
+	var x_unit = Math.floor(canv_x_pos / (WIDTH / NUM_X));
+	var y_unit = Math.floor(canv_y_pos / (HEIGHT / NUM_Y));
+	//console.log(x_unit + "," + y_unit);
+	increment(x_unit,y_unit);
+}, false);
 
 function drawXGlyph(x_pos, y_pos)
 {
@@ -56,13 +73,13 @@ function drawXGlyph(x_pos, y_pos)
 	ctx.beginPath();
 	ctx.moveTo(x_start + x_offset, y_start + y_offset);
 	ctx.lineTo((x_start * 3) + x_offset, (y_start * 3) + y_offset);
-	ctx.stroke();
+	//ctx.stroke();
 	ctx.moveTo((x_start * 3) + x_offset, y_start + y_offset);
 	ctx.lineTo((x_start) + x_offset, (y_start * 3) + y_offset);
 	ctx.stroke();
 	//ctx.globalCompositeOperation = 'source-in';
 	
-    globalCompositeOperation = 'source-over';
+    //globalCompositeOperation = 'source-over';
 }
 
 function drawTexture(x_pos, y_pos, texture)
@@ -72,7 +89,7 @@ function drawTexture(x_pos, y_pos, texture)
 	var x_offset = x_unit * x_pos;
 	var y_offset = y_unit * y_pos;
     //ctx.drawImage(img, x_offset, y_offset, x_unit, y_unit);
-    console.log(textures[texture].src);
+    //console.log(texture);
     ctx.drawImage(textures[texture], x_offset,y_offset,x_unit,y_unit);
     ctx.fillStyle="rgba(0,0,0,0.5)";
     ctx.fillRect(x_offset,y_offset,x_unit,y_unit);
@@ -84,7 +101,7 @@ function drawGlyphMask()
 	{
 		for (var j = 0; j < NUM_Y; j++)
 		{
-			var randomTexture = Math.floor((Math.random() * 4));
+			//var randomTexture = Math.floor((Math.random() * 4));
 			drawXGlyph(i,j);
 		}
 	}
@@ -97,11 +114,18 @@ function drawImages()
 	{
 		for (var j = 0; j < NUM_Y; j++)
 		{
-			var randomTexture = Math.floor((Math.random() * 4));
-			drawTexture(i,j,randomTexture);
+			//var randomTexture = Math.floor((Math.random() * 4));
+			drawTexture(i,j,styleArray[i][j]);
 		}
 	}
 	//ctx.globalCompositeOperation = 'source-over';
+}
+
+function redraw()
+{
+	//drawGlyphMask();
+	drawImages();
+	setBackground();
 }
 
 
@@ -112,4 +136,15 @@ function setBackground()
 	//document.getElementsByClassName("intro")[0].style.background = "url('../grass.jpg') no-repeat 50% 50%";
 	//console.log($("section.intro").css("background-image"));
 }
+
+function increment(x_pos,y_pos)
+{
+	//console.log(styleArray);
+	styleArray[x_pos][y_pos]++;
+	styleArray[x_pos][y_pos] %= textures.length;
+	drawTexture(x_pos, y_pos, styleArray[x_pos][y_pos]);
+	setBackground();
+	//redraw();
+}
+
 
